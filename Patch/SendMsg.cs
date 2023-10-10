10c5ca621221d089500dcde29f93e2bf595c3b3f
Vyacheslav <slava.shilin07@mail.ru>
2023-10-10 20:10:17 +0300

    update

---
+++ Patch/SendMsg.cs
@@ -0,0 +1,14 @@
+﻿using EpicMMOSystem;
+
+namespace EpicMMONotifier.Patch;
+
+[HarmonyPatch]
+public static class SendMsg
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(PlayerFVX), nameof(PlayerFVX.levelUp))]
+    private static void PlayerFVXLevelUpPatch()
+    {
+        GetPlugin<Plugin>().SendDiscordMessage(Player.m_localPlayer.GetPlayerName(), LevelSystem.Instance.getLevel());
+    }
+}
